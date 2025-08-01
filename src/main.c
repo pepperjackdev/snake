5e26d15f507f8fb082c1a205211a7010f827509d
Giacomo De Florio <giacomodeflorio9093@gmail.com>
2025-08-01 21:55:50 +0200

    Added system to display game board

--- src/main.c
+++ src/main.c
@@ -1,13 +1,53 @@
 #include <raylib.h>
+#include <stdint.h>
+
+void DrawBoardGrid(int, int);
+void _DrawBoardGridColumns(int);
+void _DrawBoardGridRows(int);
 
 int main() {
     SetTargetFPS(60);
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 
-    InitWindow(600, 600, "This is Raylib!");
+    int width = 10, height = 10;
+    uint8_t board[height][width];
+
+    InitWindow(800, 800, "Snake!");
     while (!WindowShouldClose()) {
         BeginDrawing();
             ClearBackground(RAYWHITE);
+            DrawBoardGrid(width, height);
         EndDrawing();
     }
     CloseWindow();
+}
+
+void DrawBoardGrid(int numberOfColumns, int numberOfRows) {
+    _DrawBoardGridColumns(numberOfColumns);
+    _DrawBoardGridRows(numberOfRows);
+}
+
+void _DrawBoardGridColumns(int numberOfColumns) {
+    int columnSpacing = GetScreenWidth() / numberOfColumns;
+    for (int column = 1; column < numberOfColumns; column++) {
+        DrawLine(
+            column * columnSpacing, 0, 
+            column * columnSpacing, 
+            GetScreenHeight(),
+            GRAY
+        );
+    }
+}
+
+void _DrawBoardGridRows(int numberOfRows) {
+    int rowSpacing = GetScreenHeight() / numberOfRows;
+    for (int row = 1; row < numberOfRows; row++) {
+        DrawLine(
+            0,
+            row * rowSpacing,
+            GetScreenWidth(),
+            row * rowSpacing,
+            GRAY
+        );
+    }
 }
